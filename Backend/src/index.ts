8ffaa6564791db076aeb0726a839c4f68bf06461
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-11-24 18:05:31 +0530

    fix: Error correction

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,10 +5,7 @@ import { app } from "./app";
 const start = async () => {
   try {
     // Connect to MongoDB
-    await mongoose.connect(process.env.MONGO_URI!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
