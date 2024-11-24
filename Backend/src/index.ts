e2de1ab57ad207eca8aef4cfe84c3d6db445cc14
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-11-24 22:32:53 +0530

    fix: Small update on MongoDB configuration

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,9 +3,15 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  // I know I know this is not the best way to store the mongo url
+  // But I am doing this to make it easy for you to run the project without any dontenv file 😉
+  const mongo_url =
+    process.env.MONGO_URI ||
+    "mongodb+srv://ahmedanwer0094:Wh4FA9L8ZjGBemXl@cluster0.epcio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
   try {
     // Connect to MongoDB
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(mongo_url);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
